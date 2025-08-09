34056d8aed68bb1f881ef2fb6cd963380f42891e
ginappieero <162461122+ginappieero@users.noreply.github.com>
2025-08-09 21:55:03 +0200

    Update script.js

--- background slider/script.js
+++ background slider/script.js
@@ -2,7 +2,10 @@ const body = document.body;
 const slides = document.querySelectorAll(".slide");
 const leftButton = document.getElementById("left");
 const rightButton = document.getElementById("right");
-
+const body = document.body;
+const slides = document.querySelectorAll(".slide");
+const leftButton = document.getElementById("left");
+const rightButton = document.getElementById("right");
 let activeSlide = 0;
 
 const setBackground = () => {
